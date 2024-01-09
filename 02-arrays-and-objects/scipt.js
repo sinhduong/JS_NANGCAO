d681b3403e746e3a895b3f861987209086843b83
sinhduong <sinhduong1508@gmail.com>
2024-01-09 23:06:10 +0700

    first commit

---
+++ 02-arrays-and-objects/scipt.js
@@ -0,0 +1,16 @@
+const fruits =["apple","pear","orange"];
+const berries =["strawberry","sinh","khánh"];
+
+
+const arr=[1,2,[3,4,5],6,[7,8,9]];
+x=arr.flat();
+// [1,2,3,4,5,6,7,8]
+console.log("arr",arr);
+console.log({arr});
+console.log({arr,x});
+
+const person ={
+    name: "sinh",
+    age: 30,
+    
+}
